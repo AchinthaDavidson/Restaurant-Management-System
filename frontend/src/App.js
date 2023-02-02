ea09bf3a8c983c217d05073f8cf53d4956dbb0b0
IT21373770 <it21373770@my.sliit.lk>
2023-02-02 23:34:22 +0530

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard/Dashboard.jsx';
 import Order from './pages/Order/Order.jsx';
-import Restaurant from './pages/Resturent/Restaurant.jsx';
+import Restaurant from './pages/Restaurant/Restaurant.jsx';
+import RestaurantAdd from './pages/Restaurant/RestaurantAdd';
+import RestaurantDelete from './pages/Restaurant/RestaurantDelete';
 import Bar from './pages/Bar/Bar.jsx';
 import Food from './pages/Food/Food.jsx';
 import Waiter from './pages/Waiter/Waiter.jsx';
 import QandA from './pages/Q&A/QandA.jsx';
 import Menu from './pages/Menu/Menu';
+import AddMenu from './pages/Menu/addMenu';
 import Driver from './pages/Driver/Driver.jsx';
 import History from'./pages/Order/history.jsx';
 // import Loginfrom from './components/logn';
@@ -26,10 +29,13 @@ const App = () => {
           <Route path="/Order/history" element={<History />} />
           <Route path="/Bar" element={<Bar />} />
           <Route path="/Restaurant" element={<Restaurant/>} />
+          <Route path="/Restaurant/RestaurantAdd" element={<RestaurantAdd/>} />
+          <Route path="/Restaurant/RestaurantDelete" element={<RestaurantDelete/>} />
           <Route path="/Food" element={<Food />} />
           <Route path="/Waiter" element={<Waiter />} />
           <Route path="/QandA" element={<QandA />} />
           <Route path="/Menu" element={<Menu />} />
+          <Route path="/Menu/addMenu" element={<AddMenu/>} />
           <Route path="/Driver" element={<Driver />} />
           <Route path='/somewhere' render={() => <h2>You Are Somewhere</h2>} />
         </Routes>
