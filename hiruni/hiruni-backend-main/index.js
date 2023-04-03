58fab9f07b690a4124b2a5913688ce9a6c76ef98
it21367458 <99566405+it21367458@users.noreply.github.com>
2023-04-03 12:45:34 +0530

    Login
    .

---
+++ hiruni/hiruni-backend-main/index.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./src/routes/auth.route");
+const userRoutes = require("./src/routes/user.route");
+const { MONGO_URL, PORT } = require("./src/configs/server");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+require("./src/middlewares/passport.middleware");
+
+const app = express();
+const port = PORT;
+
+// set up cors to allow us to accept requests from our client
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// connect to mongoDB
+main().catch((err) => console.log(err));
+async function main() {
+  mongoose.set("strictQuery", false);
+  await mongoose.connect(MONGO_URL);
+}
+
+app.use("/auth", authRoutes);
+app.use("/user", passport.authenticate("jwt", { session: false }), userRoutes);
+
+app.listen(port, () => {
+  console.log(`server started in port ${port}`);
+});
+
+// Export the Express API
+module.exports = app;
