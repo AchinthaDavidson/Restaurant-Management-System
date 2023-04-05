4b8e7efa5ca07342f7a7c10c8e5961825818778e
IT21209352 <it21209352@my.sliit.lk>
2023-04-06 00:38:40 +0530

    Added all the basic functions of the map

--- frontend/src/pages/Map/map.jsx
+++ frontend/src/pages/Map/map.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import axios from "axios";
 import Niv from '../../components/Niv';
-import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, Marker, useJsApiLoader, Autocomplete,DirectionsRenderer } from '@react-google-maps/api';
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./map.css"
@@ -10,57 +10,154 @@ const center2 = {lat:6.949,lng:80.789};
 
 export default function DriverMap() {
 
-    const center = { lat: 48.8584, lng:2.2945}
+ 
+
+    const [map,setMap] = useState(/** @type google.maps.Map  */ (null))
+    const [directionResponse, setDirectionResponse] = useState("")
+    const [distance,setDistance] = useState("")
+    const [duration,setDuration] = useState("")
+
+    /** @type React.MutableRefObject<HTMLInputElement> */
+    const originRef = useRef()
+
+    const permOrigin = "Nuwara Eliya, Sri Lanka"
+
+     /** @type React.MutableRefObject<HTMLInputElement> */
+    const destinationRef = useRef()
+
+    const center = useMemo(() => ({lat:6.949632874163033,lng:80.78905943343568}), []);
 
     const {isLoaded} = useJsApiLoader({
-      googleMapsApiKey: 'AIzaSyBv1H_VqIZse7f0hBdvLJThzpB-SaFfkPg'
+      googleMapsApiKey: 'AIzaSyBv1H_VqIZse7f0hBdvLJThzpB-SaFfkPg',
+      libraries:['places']
     });
   
+    async function calculateRoute(){
+       
+        // if(originRef.current.value === '' || destinationRef.current.value ===''){
+        //     toast.error("Please enter both pickup and deliver locations...")
+        //     return
+        // }
+
+        if(destinationRef.current.value ===''){
+            toast.error("Please deliver locations...")
+            return
+        }
+
+         //eslint-disable-next-line no-undef
+        const directionService = new google.maps.DirectionsService()
+        const results = await directionService.route({
+            origin:  permOrigin ,
+           // origin: originRef.current.value,
+            destination: destinationRef.current.value,
+            //eslint-disable-next-line no-undef
+            travelMode : google.maps.TravelMode.DRIVING
+
+        })
+        setDirectionResponse(results)
+        setDistance(results.routes[0].legs[0].distance.text)
+        setDuration(results.routes[0].legs[0].duration.text)
+       
+     }
+
+     function clearRoutes(){
+        setDirectionResponse(null)
+        setDistance("")
+        setDuration("")
+        //originRef.current.value = ''
+       // originRef.current.value = ''
+        destinationRef.current.value = ''
+        window.location.reload()
+     }
+
     if(!isLoaded) return <div><h1>Loading</h1></div>;
 
         return(
             <div>
-            <Niv name='Driver'/>
+            <Niv name='Driver Map'/>
             <ToastContainer position="top-right" theme="colored" /> 
             <div className="data">
                
-               
-
-                <table style={{ width:"100%", margin:"auto auto" }}>
+              <table style={{ width:"100%", margin:"auto auto" }}>
                     <tbody>
-        
                         <tr>
-                            <td rowSpan={4} style={{ width:"100%", margin:"auto auto" , minWidth:"50vh" }} >
-                                <div className="positionDiv">
-                                    <Map />
-                                </div>
+                        <td rowSpan={5} style={{ width:"70%", margin:"auto auto" , minWidth:"50vh" }} >
+                            <div className="positionDiv" style={{margin:"auto auto"}}>
+                            <GoogleMap 
+                                zoom={15} 
+                                center={center2} 
+                                mapContainerClassName="mapContainer" 
+                                id="map"
+                                mapContainerStyle={{margin:"äuto auto"}}
+                                onLoad={map => setMap(map)}
+                                options={{
+                                
+                                    streetViewControl:false,
+                                    mapTypeControl:false,
+                                    fullscreenControl:false
+
+                                }}>    
+                                <Marker position={center2} />
+
+                                {directionResponse && <DirectionsRenderer directions={directionResponse}/>} 
+    
+                            </GoogleMap>
+                            </div>
                             </td>
-                            <td><input type="text" name="" id="" placeholder=" Start Location"  style={{height:"3rem"}}/></td>
-                            <td><input type="text" name="" id="" placeholder=" Destination"  style={{height:"3rem"}}/></td>
-                        
+
+                            <td colSpan={2}>
+                            {/* <Autocomplete>
+                                <input type="text" 
+                                    placeholder="Enter your pickup point..."  
+                                    style={{height:"4rem", padding:"2rem 2rem 2rem 2rem ", width:"100%"}}
+                                    ref={originRef}/>
+                            </Autocomplete>    */}
+                            <p>PickUp Location ; {permOrigin}</p>
+                            </td>
+                            
+                        </tr>
+                        <tr>
+                            <td colSpan={2}>
+                            <Autocomplete>
+                                <input type="text"  
+                                placeholder="Enter your drop point..."  
+                                style={{height:"4rem", padding:"2rem 2rem 2rem 2rem ", width:"100%"}}
+                                ref={destinationRef}/>
+                            </Autocomplete>
+                            </td>
                         </tr>
                       
         
                         <tr>
-                        <td><p>Distance :</p></td>
-                            <td><p>Duration :</p></td>
+                        <td><p>Distance : {distance}</p></td>
+                            <td><p>Duration : {duration}</p></td>
                             
                         </tr>
                         <tr>
                             <td colSpan={2}>
                             <button 
-                                className="middlebtns2" >   
+                                className="middlebtns2" 
+                                onClick={calculateRoute}>   
                                 Calculate Route
                             </button>  
                             </td>
 
                         </tr>
                         <tr>
-                            <td colSpan={2}>
-                            <button 
-                                className="middlebtns2" >   
-                                Clear
-                            </button>  
+                            <td>
+                                <button 
+                                    className="middlebtns2" 
+                                    onClick={clearRoutes}>   
+                                    Clear
+                                </button>  
+                            </td>
+                            <td>
+                                <button 
+                                    className="middlebtns2" 
+                                    onClick={() => map.panTo(center)}
+                                    >   
+                                   Center
+                                </button>  
                             </td>
                         </tr>
                     </tbody>
@@ -73,27 +170,13 @@ export default function DriverMap() {
 
          
 
-  function Map(){
-    const center = useMemo(() => ({lat:6.949,lng:80.789}), []);
+//   function Map(){
+//     const center = useMemo(() => ({lat:6.949,lng:80.789}), []);
    
-    return (
-    <GoogleMap 
-        zoom={15} 
-        center={center} 
-        mapContainerClassName="mapContainer" 
-        id="map"
-        mapContainerStyle={{margin:"äuto auto"}}
-        options={{
-         
-            streetViewControl:false,
-            mapTypeControl:false,
-            fullscreenControl:false
-        }}
-      >    
-      <Marker position={center2} />
-      <Marker position={{lat:6.949,lng:80.789}} />
+//     return (
+        
 
-    </GoogleMap>)
-}
+//     )
+// }
 
   
