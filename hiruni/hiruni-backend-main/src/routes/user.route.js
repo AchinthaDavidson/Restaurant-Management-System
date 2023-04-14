fca5e308268f0998c5860f74b3e1a0301b406f79
hiruni-maker <121105405+hiruni-maker@users.noreply.github.com>
2023-04-14 06:13:32 +0530

    Delete hiruni directory
    done

--- hiruni/hiruni-backend-main/src/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/user.controller");
-
-router.get("/details", userController.getUser);
-router.put("/details", userController.updateUser);
-
-module.exports = router;
