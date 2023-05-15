914bc3381d1e0bde195b9e0954a153deb8712a7f
IT21294952 <it21294952@my.sliit.lk>
2023-05-15 19:08:49 +0530

    Cart issues made

---
+++ Customer/frontend/src/components/CartItem.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { RiDeleteBin6Line } from "react-icons/ri";
+export default function CartItem({product, onProductRemove, onQuantityChange}) {
+  return (
+    <div
+        className="cart-product"
+        key={product.id}>
+        <img
+            src={
+                product.image
+            }
+            alt={product.name}
+        />
+        <div className="product-info">
+            <h3>
+                {product.name}
+            </h3>
+            <span className="product-price">
+                Rs. {product.price *
+                    product.count}
+                
+            </span>
+        </div>
+        <input type= "number" className="count" min="1" max="100" pattern="[0-9]"
+            value={
+                product.count
+            }
+            onChange={(
+                event
+            ) => {
+                onQuantityChange(
+                    product.id,
+                    event
+                        .target
+                        .value
+                );
+            }}>
+          
+        </input>
+        <button
+            className="btn remove-btn"
+            onClick={() =>
+                {onProductRemove(
+                    product.id
+                )}
+            }>
+            <RiDeleteBin6Line
+                size={20}
+            />
+        </button>
+    </div>
+  )
+}
--- Customer/frontend/src/screens/Menu/shoppingcart.js
+++ Customer/frontend/src/screens/Menu/shoppingcart.js
@@ -1,8 +1,119 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import "./shoppingcart.css";
+// import { IoMdCloseCircle } from "react-icons/io";
+// import { RiDeleteBin6Line } from "react-icons/ri";
+// import { useNavigate } from "react-router-dom";
+
+// function ShoppingCart({
+// 	visibilty,
+// 	products,
+// 	onProductRemove,
+// 	onClose,
+// 	onQuantityChange,
+// })
+// {
+// 	const handleButtonClick = () => {
+// 			let res = JSON.parse(localStorage.getItem("userData"));
+// 			if (!res) {
+// 				window.location.href = "/Signin"
+// 			}
+// 			else{
+
+// 			}
+// 	}
+
+// 	return (
+// 		<div
+// 			className="model"
+// 			style={{
+// 				display: visibilty
+// 					? "block"
+// 					: "none",
+// 			}}>
+// 			<div className="shoppingCart">
+// 				<div className="header">
+// 					<h2>Shopping cart</h2>
+// 					<button
+// 						className="btn close-btn"
+// 						onClick={onClose}>
+// 						<IoMdCloseCircle
+// 							size={30}
+// 						/>
+// 					</button>
+// 				</div>
+// 				<div className="cart-products">
+// 					{products.length === 0 && (
+// 						<span className="empty-text">
+// 							Your cart is
+// 							currently empty
+// 						</span>
+// 					)}
+// 					{products.map((product) => (
+// 						<div
+// 							className="cart-product"
+// 							key={product.id}>
+// 							<img
+// 								src={
+// 									product.ImageURL}
+// 								alt={product.Name}
+// 							/>
+// 							<div className="product-info">
+// 								<h3>
+// 									{product.Name}
+// 								</h3>
+// 								<span className="product-price">
+// 									Rs. {product.Price *
+// 										product.count}
+									
+// 								</span>
+// 							</div>
+// 							<input type= "number" className="count" min="1" max="100" pattern="[0-9]+"
+// 								value={
+// 									product.count
+// 								}
+// 								onChange={(
+// 									event
+// 								) => {
+// 									onQuantityChange(
+// 										product.id,
+// 										event.target.value
+// 									);
+// 								}}>
+								
+// 							</input>
+// 							<button
+// 								className="btn remove-btn"
+// 								onClick={() =>
+// 									onProductRemove(
+// 										product
+// 									)
+// 								}>
+// 								<RiDeleteBin6Line
+// 									size={20}
+// 								/>
+// 							</button>
+// 						</div>
+// 					))}
+// 					{products.length > 0 && (
+// 						<a href="/OrderSummary">
+// 						<button className="btn checkout-btn" onClick={handleButtonClick}>
+// 							Proceed to checkout
+// 						</button>
+// 						</a>
+// 					)}
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// export default ShoppingCart;
+
+import React from "react";
 import "./shoppingcart.css";
 import { IoMdCloseCircle } from "react-icons/io";
-import { RiDeleteBin6Line } from "react-icons/ri";
-import { useNavigate } from "react-router-dom";
+
+import CartItem from "../../components/CartItem";
 
 function ShoppingCart({
 	visibilty,
@@ -10,21 +121,10 @@ function ShoppingCart({
 	onProductRemove,
 	onClose,
 	onQuantityChange,
-})
-{
-	const handleButtonClick = () => {
-			let res = JSON.parse(localStorage.getItem("userData"));
-			if (!res) {
-				window.location.href = "/Signin"
-			}
-			else{
-
-			}
-	}
-
+}) {
 	return (
 		<div
-			className="model"
+			className="modal"
 			style={{
 				display: visibilty
 					? "block"
@@ -49,57 +149,12 @@ function ShoppingCart({
 						</span>
 					)}
 					{products.map((product) => (
-						<div
-							className="cart-product"
-							key={product.id}>
-							<img
-								src={
-									product.ImageURL}
-								alt={product.Name}
-							/>
-							<div className="product-info">
-								<h3>
-									{product.Name}
-								</h3>
-								<span className="product-price">
-									Rs. {product.Price *
-										product.count}
-									
-								</span>
-							</div>
-							<input type= "number" className="count" min="1" max="100" pattern="[0-9]+"
-								value={
-									product.count
-								}
-								onChange={(
-									event
-								) => {
-									onQuantityChange(
-										product.id,
-										event.target.value
-									);
-								}}>
-								
-							</input>
-							<button
-								className="btn remove-btn"
-								onClick={() =>
-									onProductRemove(
-										product
-									)
-								}>
-								<RiDeleteBin6Line
-									size={20}
-								/>
-							</button>
-						</div>
+						<CartItem product={product} onQuantityChange={onQuantityChange} onProductRemove={onProductRemove}/>
 					))}
 					{products.length > 0 && (
-						<a href="/OrderSummary">
-						<button className="btn checkout-btn" onClick={handleButtonClick}>
+						<button className="btn checkout-btn">
 							Proceed to checkout
 						</button>
-						</a>
 					)}
 				</div>
 			</div>
@@ -108,3 +163,4 @@ function ShoppingCart({
 }
 
 export default ShoppingCart;
+
