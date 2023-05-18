750b196e5cc5adc1e1391a1f4ecdc675ebe279e2
IT21209352 <it21209352@my.sliit.lk>
2023-05-18 14:03:27 +0530

    locationinCheckout

--- Customer/frontend/src/screens/Menu/shoppingcart.js
+++ Customer/frontend/src/screens/Menu/shoppingcart.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./shoppingcart.css";
 import { IoMdCloseCircle } from "react-icons/io";
-import { GoogleMap, Marker, useJsApiLoader, Autocomplete,DirectionsRenderer } from '@react-google-maps/api';
 import { useState, useRef, useEffect } from "react";
 import CartItem from "../../components/CartItem";
+import {
+	GoogleMap,
+	Marker,
+	useJsApiLoader,
+	Autocomplete,
+	DirectionsRenderer,
+} from "@react-google-maps/api";
+
+const libraries = ["places"];
 
 function ShoppingCart({
 	visibilty,
@@ -11,10 +19,13 @@ function ShoppingCart({
 	onProductRemove,
 	onClose,
 	onQuantityChange,
-}) 
-{
+}) {
 	const [address, setAddress] = useState("");
-
+	const { isLoaded } = useJsApiLoader({
+		googleMapsApiKey: process.env.REACT_APP_GOOGLEMAP_API_KEY,
+		libraries,
+	});
+	if(!isLoaded) return <div><h1>Loading</h1></div>;
 	return (
 		<div
 			className="modal"
@@ -41,20 +52,34 @@ function ShoppingCart({
 							currently empty
 						</span>
 					)}
-					{products.map((product) => (
-						<CartItem product={product} onQuantityChange={onQuantityChange} onProductRemove={onProductRemove}/>
+					{products.map((product,index) => (
+						<CartItem product={product} key={index} onQuantityChange={onQuantityChange} onProductRemove={onProductRemove}/>
 					))}
 					<hr/>
-					<div className="input-field">
-						<label className="PhoneNo">Phone number</label>
-						<input type="text" placeholder="Phone Number" pattern="[0-9]{10}"
-						title="Enter valied phone number"/>
-					</div>
-					<div className="input-field">
-						<label className="Address">Current Address</label>
+				<div >
+						
+					<input 
+						style={{ padding: "1rem" , backgroundColor:"white", color:"black" , margin:"auto auto"}}
+						type="text" placeholder="Enter Your Phone Number Here " pattern="[0-9]{10}"
+						title="Enter valid phone number"/>
 
-					</div>
+					 <Autocomplete		
+						z-index={50}
+						onChange={(e) => setAddress(e.target.value)}
+						style={{ padding: "1rem" , backgroundColor:"white", color:"black" }}>
+                               <input
+							   z-index-={101}
+                                  type="text"
+                                  hidden={false}
+                                  id="locationInputs"
+                                  style={{ padding: "1rem" , backgroundColor:"white", color:"black"}}
+                                  placeholder="Enter your drop point..."
+                                  onClick={(e) => setAddress(e.target.value)}  
+								  defaultValue={address}    /> 
 
+                     </Autocomplete> 
+					</div>
+					<div style={{margin:"auto auto"}}>
 					{products.length > 0 && (
 						<a href="/OrderSummary">
 						<button className="btn checkout-btn">
@@ -62,6 +87,10 @@ function ShoppingCart({
 						</button>
 						</a>
 					)}
+						
+					</div>
+
+				
 				</div>
 			</div>
 		</div>
@@ -69,4 +98,3 @@ function ShoppingCart({
 }
 
 export default ShoppingCart;
-
