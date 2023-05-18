6e2fd7373ed5d0a4337e5ca166c8aa0838ab1228
it21367458 <121105405+hiruni-maker@users.noreply.github.com>
2023-05-18 23:43:35 +0530

    delete profile done
    delete complete

--- Customer/backend/src/controllers/user.controller.js
+++ Customer/backend/src/controllers/user.controller.js
@@ -275,20 +275,19 @@ exports.getdish=(req, res) => {
 })
 
 }
-
-exports.deleteUser = (req,res)=>{
-  User.findOneAndDelete({_id:req.user_id})
-  .then((user) =>{
-    if(user) {
-      res.status(200).json({message:"success"});
-
-    }else{
-      res.status(400).json({message:"failed"});
-    }
-
-  })
-  .catch((err) =>{
-    console.log(err);
-    res.status(500).json({message:"server error"});
-  });
+exports.deleteUser = (req, res) => {
+  User.findOneAndDelete({ _id: req.user._id })
+    .then((user) => {
+      if (user) {
+        res.status(200).json({ message: "success" });
+      } else {
+        res.status(400).json({ message: "failed" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "server error" });
+    });
 };
+
+
